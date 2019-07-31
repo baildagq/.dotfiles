5071625cd3ba60bec9e73fa4df121edd247497c7
qistout <2817253603@qq.com>
2019-07-31 14:27:37 +0800

    small modified

--- script/install.sh
+++ script/install.sh
@@ -12,6 +12,9 @@ sudo ln -s ~/.config_file/sources.list /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade -y
 
+#========terminal
+sudo apt install -y alacritty 
+# sudo apt install -y rxvt-unicode
 
 #========install important tools========
 sudo apt install -y --upgrade git zsh vimt tmux ncurses-* ### very important!!!!! solve tmux problem
@@ -19,14 +22,10 @@ sudo apt install -y --upgrade git zsh vimt tmux ncurses-* ### very important!!!!
 #========install basic tools========
 sudo apt install -y tree curl tweak tldr ctags cscope build-essential flameshot silversearcher-ag gnome-nettool cmatrix fortune-mod 
 
-# #========install google chrome========
-# sudo dpkg -i ~/.config_file/software_packges/google-chrome.deb
-
 #========install sogoupinyin========
  curl  http://cdn2.ime.sogou.com/dl/index/1524572264/sogoupinyin_2.2.0.0108_amd64.deb\?st\=e2rkzoJ2HibQ0Y8dfcZkUQ\&e\=1550853634\&fn\=sogoupinyin_2.2.0.0108_amd64.deb --create-dirs -o ~/tmp/software_packges/sogoupinyin.deb
 sudo apt install -y --upgrade fctix
 sudo dpkg -i ~/tmp/software_packges/sogoupinyin.deb
-
 
 #========install wps-office========
 curl http://kdl.cc.ksosoft.com/wps-community/download/6757/wps-office_10.1.0.6757_amd64.deb --create-dirs -o ~/tmp/software_packges/wps-office.deb
@@ -37,13 +36,12 @@ sudo apt-get purge -y libreoffice*
 sudo apt autoremove -y
 sudo apt install -y -f 
 
-
-#========install sublime =============
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update -y
-sudo apt-get install -y sublime-text
+# #========install sublime =============
+# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+# sudo apt-get install apt-transport-https
+# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+# sudo apt-get update -y
+# sudo apt-get install -y sublime-text
 
 #========install typora========
 wget -qO - https://typora.io/linux/public-key.asc | apt-key add -
@@ -54,7 +52,6 @@ sudo apt-get install -y typora
 #========install texlive=========
 sudo apt install -y texlive-full 
 
-
 #========update pip========
 sudo apt install -y python3-pip
 
@@ -62,4 +59,3 @@ sudo apt install -y python3-pip
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions  #install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting #install zsh-syntax-hightlighting
-
--- script/makeSoftLinks.sh
+++ script/makeSoftLinks.sh
@@ -7,9 +7,12 @@
 
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/
 ln -s ~/dotfiles/zsh/.zshrc_wm ~/.zshrc
 ln -s ~/dotfiles/.Xresources ~/.Xresources
-ln -s ~/dotfiles/
-ln -s ~/dotfiles/
-ln -s ~/dotfiles/
+ln -s ~/dotfiles/terminal/urxvt ~/.urxvt
+
+ln -s ~/dotfiles/windowsManager/i3 ~/.config/i3
+ln -s ~/dotfiles/windowsManager/compton.conf ~/.config/compton.conf
+ln -s ~/dotfiles/feh               ~/.config/feh
+ln -s ~/dotfiles/zathura           ~/.config/zathura
+ln -s ~/dotfiles/terminal/alacritty ~/.config/alacritty
