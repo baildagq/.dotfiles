2d9a7dabb1b011ee26c4c7aff558f8bfd94817da
qistout <2817253603@qq.com>
2019-09-11 20:00:18 +0800

    update

--- script/makeSoftLinks.sh
+++ script/makeSoftLinks.sh
@@ -5,16 +5,17 @@
 # Created Time: 2019-07-28 18:12:36 #
 #####################################
 
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/zsh/.zshrc_wm ~/.zshrc
-ln -s ~/dotfiles/.Xresources ~/.Xresources
-ln -s ~/dotfiles/terminal/urxvt ~/.urxvt
+ln -s ~/.dotfiles/.vim              ~/.vim
+ln -s ~/.dotfiles/.tmux.conf        ~/.tmux.conf
+ln -s ~/.dotfiles/zsh/.zshrc_zpl    ~/.zshrc
+ln -s ~/.dotfiles/.Xresources       ~/.Xresources
+ln -s ~/.dotfiles/.gitconfig        ~/.gitconfig
 
-ln -s ~/dotfiles/windowsManager/i3 ~/.config/i3
-ln -s ~/dotfiles/windowsManager/compton.conf ~/.config/compton.conf
-ln -s ~/dotfiles/windowsManager/dunst ~/.config/dunst
+ln -s ~/.dotfiles/i3                ~/.config/i3
+ln -s ~/.dotfiles/compton.conf      ~/.config/compton.conf
+ln -s ~/.dotfiles/dunst             ~/.config/dunst
 
-ln -s ~/dotfiles/feh               ~/.config/feh
-ln -s ~/dotfiles/zathura           ~/.config/zathura
-ln -s ~/dotfiles/terminal/alacritty ~/.config/alacritty
+ln -s ~/.dotfiles/feh               ~/.config/feh
+ln -s ~/.dotfiles/zathura           ~/.config/zathura
+ln -s ~/.dotfiles/alacritty         ~/.config/alacritty
+ln -s ~/.dotfiles/pip               ~/.config/pip
