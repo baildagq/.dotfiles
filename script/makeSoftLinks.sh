107099f85a5baf50110418befc713a769b402689
qistout <2817253603@qq.com>
2019-09-16 03:58:24 +0800

    update

--- script/makeSoftLinks.sh
+++ script/makeSoftLinks.sh
@@ -5,20 +5,35 @@
 # Created Time: 2019-07-28 18:12:36 #
 #####################################
 
-ln -s ~/.dotfiles/.vim              ~/.vim
-ln -s ~/.dotfiles/.tmux.conf        ~/.tmux.conf
-ln -s ~/.dotfiles/zsh/.zshrc_zpl    ~/.zshrc
-ln -s ~/.dotfiles/.Xresources       ~/.Xresources
-ln -s ~/.dotfiles/.gitconfig        ~/.gitconfig
-ln -s ~/.dotfiles/.condarc          ~/.condarc
 
-ln -s ~/.dotfiles/i3                ~/.config/i3
-ln -s ~/.dotfiles/compton.conf      ~/.config/compton.conf
-ln -s ~/.dotfiles/dunst             ~/.config/dunst
-
-ln -s ~/.dotfiles/feh               ~/.config/feh
-ln -s ~/.dotfiles/zathura           ~/.config/zathura
 ln -s ~/.dotfiles/alacritty         ~/.config/alacritty
+ln -s ~/.dotfiles/codestyle/pycodestyle       ~/.config/pycodestyle
+ln -s ~/.dotfiles/codestyle/pep8       ~/.config/pep8
+ln -s ~/.dotfiles/codestyle/flake8      ~/.config/flake8
+
+ln -s ~/.dotfiles/.condarc          ~/.condarc
+ln -s ~/.dotfiles/dunst             ~/.config/dunst
+ln -s ~/.dotfiles/feh            ~/.config/feh
+
+ln -s ~/.dotfiles/.gitconfig        ~/.gitconfig
+ln -s ~/.dotfiles/i3                ~/.config/i3.back
+ln -s ~/.dotfiles/openvpn          ~/.config/openvpn
+
+
 ln -s ~/.dotfiles/pip               ~/.config/pip
-ln -s ~/.dotfiles/pep8              ~/.config/pep8
-ln -s ~/.dotfiles/pycodestyle       ~/.config/pycodestyle
+ln -s ~/.dotfiles/polybar              ~/.config/polybar
+
+ln -s ~/.dotfiles/ranger            ~/.config/ranger
+
+ln -s ~/.dotfiles/.tmux.conf        ~/.tmux.conf
+
+
+ln -s ~/.dotfiles/.vim              ~/.vim
+
+ln -s ~/.dotfiles/.xmodmap       ~/.xmodmap
+ln -s ~/.dotfiles/.xprofile       ~/.xprofile
+ln -s ~/.dotfiles/.Xresources       ~/.Xresources
+
+
+ln -s ~/.dotfiles/zathura           ~/.config/zathura
+ln -s ~/.dotfiles/zsh/.zshrc_zpl    ~/.zshrc
---
+++ script/manjaronInstall.sh
@@ -0,0 +1,12 @@
+# pacman
+sudo pacman -Su yay git vim zsh tmux ranger zathura nodejs npm yarn ruby-rouge ripgrep i3 dunst compton variety brightnessctl fcitx fcitx-googlepinyin yay the_silver_searcher python-pycodestyle flameshot polybar openvpn openssh neofetch
+
+# yay 
+
+# pip
+pip3 install -U --user pynvim
+
+# sh
+bash ~/Downloads/Miniconda3-latest-Linux-x86_64.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+chsh -s /usr/bin/zsh
